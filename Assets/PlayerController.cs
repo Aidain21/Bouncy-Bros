951de0508f15509b96dbdec5b62e12b31991eb6f
Aidan Ainsley <aidanainsley@gmail.com>
2024-09-20 17:30:34 -0400

    a

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -17,6 +17,19 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        
+        GetComponent<Rigidbody2D>().velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        if (Input.GetKey(KeyCode.A))
+        {
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //float dirY = GetComponent<Rigidbody2D>().velocity.y;
+            playerRB.velocity = new Vector3(GetComponent<Rigidbody2D>().velocity.x, jumpSpeed);
+        }
     }
 }
