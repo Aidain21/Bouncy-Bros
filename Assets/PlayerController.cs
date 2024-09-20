59403a1d9cb66afa88cc45d51a3295af02fcef6e
NateBob <nshafer210@gmail.com>
2024-09-20 17:21:59 -0400

    awesome

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
     public float jumpSpeed = 6f;
     [HideInInspector] public BoxCollider2D playerCol;
     [HideInInspector] public Rigidbody2D playerRB;
+    public float GroundTestHeight = 0.1f;
+    public LayerMask platformLayerMask;
 
     private void Awake()
     {
@@ -18,5 +20,12 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate()
     {
         
+    }
+
+    private bool IsGrounded()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(playerCol.bounds.center, playerCol.bounds.size, 0f, Vector2.down, GroundTestHeight, platformLayerMask);
+
+        return raycastHit.collider != null;
     }
 }
